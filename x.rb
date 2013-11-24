f0599e973799420d57ebbe070487d96f0c2637e7
chadnash <chad@nash.org>
2013-11-24 13:06:50 +1100

    tyy two classes in one file

--- x.rb
+++ x.rb
@@ -8,6 +8,20 @@ def initialize(name = "World")
 def sayBye
    puts "Bye #{@name}, come back soon."
  end
-g = X.new("Pat")
-g.sayHi
+
 end
+class Z
+  def initialize(name = "World")
+    @name = name
+  end
+  def sayHi
+    puts "Hi #{@name}!"
+  end
+  def sayBye
+    puts "Bye #{@name}, come back soon."
+  end
+
+end
+g = Z.new "Pat"
+g.sayHi
+
---
+++ y.rb
@@ -0,0 +1,3 @@
+class Y
+  # To change this template use File | Settings | File Templates.
+end
