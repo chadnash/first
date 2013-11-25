7a0d5e936076f38d6603663b89876eb7d9bd6901
chadnash <chad@nash.org>
2013-11-25 23:14:54 +1100

    more tests removed capturing stdout in tests put in mainline that runs from stdin to stdout

--- simple.rb
+++ simple.rb
@@ -3,6 +3,17 @@
 
 class Robot
   North,South,East,West="NORTH","SOUTH","EAST","WEST"
+
+  def self.run
+    r= self.new
+    ARGF.each_line do |cmd|
+      report= r.executeCommand(cmd)
+      puts report unless report.nil?
+    end
+
+
+  end
+
   def initialize()
     @isOnTable=false
     @reports=nil
@@ -18,7 +29,7 @@ class Robot
           when East then   North
           when West then   South
         end
-
+    return nil
   end
 
   def rightIfOnTable
@@ -36,7 +47,7 @@ class Robot
     #@heading=West if @heading==South
     #@heading=South if @heading==East
     #@heading=North if @heading==West
-
+    return nil
   end
 
   def moveIfOnTableAndStaysOnTable
@@ -53,12 +64,14 @@ class Robot
       proposedX, proposedY=proposedX-1, proposedY
     end
     @x, @y =constrainedX(proposedX), constrainedY(proposedY)
+    return nil
   end
 
   def placeIfItWillBeOntoTable(x, y, heading)
     if x==constrainedX(x) && y==constrainedY(y)
       @x, @y, @heading, @isOnTable= x, y, heading, true
     end
+    return nil
   end
 
   def reportIfOnTable()
@@ -66,17 +79,23 @@ class Robot
 
     report =  "#@x,#@y,#@heading"
 
-
-    puts  report
+    return report
   end
  
   def executeCommands(commands)
-    if commands.respond_to?("each") then
-        commands.each {  |command| executeCommand command     }
-    elsif commands.respond_to?("each_line") then
-      commands.each_line {  |command| executeCommand command     }
-    end
+    reports=[]
 
+    if commands.respond_to?("each_line") then
+      return executeCommands commands.split("\n")
+    end
+    commands.each {
+        |command|
+      report = executeCommand command
+      (reports << report) unless report.nil?
+
+    }
+
+    return reports
   end
   def executeCommand(command )
     #puts "command=#{command}"
@@ -88,10 +107,10 @@ class Robot
     return if commandElements.size>1 && ["REPORT" , "MOVE" , "LEFT" , "RIGHT"].include?(commandName)
 
     case commandName
-      when "REPORT"  then  self.reportIfOnTable
-      when "MOVE"  then  self.moveIfOnTableAndStaysOnTable
-      when "LEFT"  then  self.leftIfOnTable
-      when "RIGHT"  then  self.rightIfOnTable
+      when "REPORT"  then  return self.reportIfOnTable
+      when "MOVE"  then  return self.moveIfOnTableAndStaysOnTable
+      when "LEFT"  then  return self.leftIfOnTable
+      when "RIGHT"  then  return self.rightIfOnTable
       when "PLACE"  then
         return if commandElements.size!= 4
         x =commandElements[1].to_i
@@ -103,7 +122,7 @@ class Robot
                     when "WEST" then West
                   end
         return  if  heading==nil || x.to_s != commandElements[1].strip || y.to_s != commandElements[2].strip
-        self.placeIfItWillBeOntoTable(x,y,heading)
+        return self.placeIfItWillBeOntoTable(x,y,heading)
     end
   end
 
@@ -126,60 +145,6 @@ class Robot
 
 end
 
-require "test/unit"
-class TestRobot  < Test::Unit::TestCase
-  StdoutStandinFilename = "out.txt"
-  def initialize(name)
-    super(name)
-    @stdoutStandIn = File.open(StdoutStandinFilename,"w")
-    $stdout=  @stdoutStandIn
-    #$stdout.reopen(StdoutStandinFilename, "w")
-  end
-  def contentOfStdoutOverTest()
-    $stdout=STDOUT
-    @stdoutStandIn.close
-    f = File.open(StdoutStandinFilename,"rb")
-    contents = f.read
-    f.close
-
-    return contents
-  end
-  def testSpecCaseA
-    r= Robot.new
-    r.executeCommands <<-EOF
-      PLACE 0,0,NORTH
-      MOVE
-      REPORT
-    EOF
-    #puts  "r.reports=#{r.reports}"
-    assert_equal(  "0,1,NORTH\r\n",contentOfStdoutOverTest )
-
-  end
-  def testSpecCaseB
-    r= Robot.new
-    r.executeCommands <<-EOF
-      PLACE 0,0,NORTH
-      LEFT
-      REPORT
-    EOF
-
-    assert_equal(  "0,0,WEST\r\n" ,contentOfStdoutOverTest )
-
-  end
-
-
-  def testSpecCaseC
-    r= Robot.new
-    r.executeCommands <<-EOF
-      PLACE 1,2,EAST
-      MOVE
-      MOVE
-      LEFT
-      MOVE
-      REPORT
-    EOF
-    assert_equal( "3,3,NORTH\r\n",contentOfStdoutOverTest )
-
-  end
+if __FILE__ == $0
+  Robot.run
 end
-
---
+++ test_robot.rb
@@ -0,0 +1,78 @@
+require "test/unit"
+require_relative "simple"
+class TestRobot  < Test::Unit::TestCase
+  @@RobotClass = Robot
+  def self.newRobot
+    return @@RobotClass.new
+  end
+  def testSpecCaseA
+    r= TestRobot.newRobot
+    reports = r.executeCommands <<-EOF
+      PLACE 0,0,NORTH
+      MOVE
+      REPORT
+    EOF
+    #puts  "r.reports=#{r.reports}"
+    assert_equal(  ["0,1,NORTH"],reports )
+
+  end
+  def testSpecCaseB
+    r= TestRobot.newRobot
+    reports=r.executeCommands <<-EOF
+      PLACE 0,0,NORTH
+      LEFT
+      REPORT
+    EOF
+
+    assert_equal(  ["0,0,WEST"] ,reports )
+
+  end
+
+
+  def testSpecCaseC
+    r= TestRobot.newRobot
+    reports=r.executeCommands <<-EOF
+      PLACE 1,2,EAST
+      MOVE
+      MOVE
+      LEFT
+      MOVE
+      REPORT
+    EOF
+    assert_equal( ["3,3,NORTH"] ,reports )
+
+  end
+  def testLimits
+    assert_equal((TestRobot.newRobot.executeCommands ["PLACE 0,1,WEST","MOVE","REPORT"]),["0,1,WEST"])
+    assert_equal((TestRobot.newRobot.executeCommands ["PLACE 1,0,SOUTH","MOVE","REPORT"]),["1,0,SOUTH"])
+    assert_equal((TestRobot.newRobot.executeCommands ["PLACE 5,1,EAST","MOVE","REPORT"]),["5,1,EAST"])
+    assert_equal((TestRobot.newRobot.executeCommands ["PLACE 1,5,NORTH","MOVE","REPORT"]),["1,5,NORTH"])
+    assert_equal((TestRobot.newRobot.executeCommands ["PLACE 1,6,NORTH","REPORT"]),[])
+    assert_equal((TestRobot.newRobot.executeCommands ["PLACE 6,0,NORTH","MOVE","REPORT"]),[])
+    assert_equal((TestRobot.newRobot.executeCommands ["PLACE -1,0,NORTH","MOVE","REPORT"]),[])
+    assert_equal((TestRobot.newRobot.executeCommands ["PLACE 6,-1,NORTH","MOVE","REPORT"]),[])
+
+  end
+  def testLeft
+    assert_equal((TestRobot.newRobot.executeCommands ["PLACE 0,1,WEST","LEFT","LEFT","LEFT","LEFT","REPORT"]),["0,1,WEST"])
+  end
+  def testRight
+    assert_equal((TestRobot.newRobot.executeCommands ["PLACE 0,1,WEST","RIGHT","RIGHT","RIGHT","RIGHT","REPORT"]),["0,1,WEST"])
+  end
+  def testRightAndLeft
+    excessOfLefts=0
+    commands =   ["PLACE 0,1,WEST"]
+   for i in 1..6
+      lefts=rand(6)
+      rights=rand(6)
+      excessOfLefts=excessOfLefts+lefts-rights
+      commands= (commands + (["LEFT"]*lefts)) + (["RIGHT"]*rights)
+    end
+    commands = commands  + (["RIGHT"]*excessOfLefts)   if excessOfLefts>0
+    commands = commands  + (["LEFT"]*(-excessOfLefts)) if    excessOfLefts<0
+    commands = commands  << "REPORT"
+    assert_equal(TestRobot.newRobot.executeCommands(commands),["0,1,WEST"])
+  end
+end
+
+
